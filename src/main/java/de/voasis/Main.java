5dda7a6b37a92d2f892f08479e8984dbae607144
aquestry <155507142+aquestry@users.noreply.github.com>
2024-10-27 14:41:52 +0100

    Update

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -13,26 +13,21 @@ import net.minestom.server.instance.block.Block;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SimpleDuels by Aquestry");
         MinecraftServer minecraftServer = MinecraftServer.init();
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
-        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 42, Block.GRASS_BLOCK));
-        String vsecret;
-
-        for (String s : args) {
-            if(s.startsWith("SECRET=")) {
-                vsecret = s.replace("SECRET=", "");
-                VelocityProxy.enable(vsecret);
-                System.out.println("v-secret: " + vsecret);
-            }
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        String vsecret = System.getenv("PAPER_VELOCITY_SECRET");
+        if(vsecret != null) {
+            VelocityProxy.enable(vsecret);
         }
-
+        System.out.println("v-secret: " + vsecret);
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             final Player player = event.getPlayer();
+            System.out.println("Player configuration event for " + player.getUsername());
             event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0, 42, 0));
+            player.setRespawnPoint(new Pos(0, 41, 0));
         });
 
         instanceContainer.setChunkSupplier(LightingChunk::new);

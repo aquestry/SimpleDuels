49a3c7d41e88db161209ec132a9919e48d6ac7bc
aquestry <155507142+aquestry@users.noreply.github.com>
2024-11-09 17:37:26 +0100

    Update Simple PVP

--- src/main/java/de/voasis/Main.java
+++ src/main/java/de/voasis/Main.java
@@ -1,25 +1,35 @@
 package de.voasis;
 
+import io.github.togar2.pvp.MinestomPvP;
+import io.github.togar2.pvp.feature.CombatFeatureSet;
+import io.github.togar2.pvp.feature.CombatFeatures;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
-import net.minestom.server.event.player.PlayerStartDiggingEvent;
+import net.minestom.server.event.player.PlayerDeathEvent;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.LightingChunk;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
 import net.minestom.server.network.packet.server.common.PluginMessagePacket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
+        MinestomPvP.init();
+
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
         instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+        CombatFeatureSet modernVanilla = CombatFeatures.modernVanilla();
+        MinecraftServer.getGlobalEventHandler().addChild(modernVanilla.createNode());
+
         if(System.getenv("PAPER_VELOCITY_SECRET") instanceof String vsecret) {
             VelocityProxy.enable(vsecret);
             System.out.println("v-secret: " + vsecret);
@@ -28,10 +38,12 @@ public class Main {
         globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
             event.setSpawningInstance(instanceContainer);
             event.getPlayer().setRespawnPoint(new Pos(0, 41, 0));
+            event.getPlayer().getInventory().addItemStack(ItemStack.builder(Material.IRON_SWORD).build());
         });
-        globalEventHandler.addListener(PlayerStartDiggingEvent.class, event -> {
-            System.out.println("Send to Lobby");
-            sendToLobby(event.getPlayer());
+        globalEventHandler.addListener(PlayerDeathEvent.class, event -> {
+            for(Player p : instanceContainer.getPlayers()) {
+                sendToLobby(p);
+            }
         });
         instanceContainer.setChunkSupplier(LightingChunk::new);
         minecraftServer.start("0.0.0.0", 25565);
